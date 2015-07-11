5019ee525f83b2950c686255e6edd2844114beff
viniciuschiele <vinicius.chiele@gmail.com>
2015-07-11 13:25:24 +0100

    Version 0.8.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sqlalchemy-multidb',
-    version='0.8.1',
+    version='0.8.2',
     packages=['sqlalchemy_multidb'],
     url='https://github.com/viniciuschiele/sqlalchemy-multidb',
     license='Apache 2.0',
